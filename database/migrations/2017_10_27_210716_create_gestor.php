15f4954fda42e1029976e86fecbf0ef36f460c62
nikolasbrandao <nikolasbrandao@gmail.com>
2017-10-27 21:25:22 -0300

    Adição da migration de gestor

--- database/migrations/2017_10_27_210716_create_gestor.php
+++ database/migrations/2017_10_27_210716_create_gestor.php
@@ -13,7 +13,20 @@ class CreateGestor extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('gestor', function(Blueprint $table){
+
+          $table->integer('codigo_imovel')->nullable($value = true);
+          $table->string('tipo_eleito',30)->nullable($value = true);
+          $table->string('cargo', 60)->nullable($value = true);
+          $table->string('situacao_cargo', 50)->nullable($value = true);
+          $table->integer('matricula')->nullable($value = true);
+          $table->string('nome', 255)->nullable($value = true);
+          $table->string('cpf', 14)->nullable($value = true);
+          $table->string('telefone', 25)->nullable($value = true);
+          $table->string('celular', 25)->nullable($value = true);
+          $table->string('celular2', 25)->nullable($value = true);
+          $table->string('email', 100)->nullable($value = true);
+        });
     }
 
     /**
@@ -23,6 +36,6 @@ class CreateGestor extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('gestor');
     }
 }
