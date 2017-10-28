60461807d37e79f54c8d90f96ca1ed204b28e6af
nikolasbrandao <nikolasbrandao@gmail.com>
2017-10-27 23:10:01 -0300

    Adição das primeiras rotas de teste para Gestor, Imovel, Repasse e Terceirizado

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/gestores', 'Api\GestorController');
+Route::resource('/imoveis', 'Api\ImovelController');
+Route::resource('/repasses', 'Api\RepasseController');
+Route::resource('/terceirizados', 'Api\TerceirizadoController');
